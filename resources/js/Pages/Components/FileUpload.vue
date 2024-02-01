93bba4ffa5a0d3e7de51a6976f6a2b96a2d0a8a2
fmercadoalv <158158154+fmercadoalv@users.noreply.github.com>
2024-02-01 12:03:22 +0100

    test

--- resources/js/Pages/Components/FileUpload.vue
+++ resources/js/Pages/Components/FileUpload.vue
@@ -78,7 +78,7 @@ const onFileChange = (event) => {
 
     const formData = new FormData()
     formData.append('file', file)
-    const url = ''
+    const url = '';
     const config = {
         headers: {
             'Content-Type': 'text/csv'
