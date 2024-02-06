b7891760bf2fb6fc478ab2f943222d181a0ab49f
fmercadoalv <158158154+fmercadoalv@users.noreply.github.com>
2024-02-06 12:48:00 +0100

    Update FileUpload.vue

--- resources/js/Pages/Components/FileUpload.vue
+++ resources/js/Pages/Components/FileUpload.vue
@@ -78,7 +78,7 @@ const onFileChange = (event) => {
 
     const formData = new FormData()
     formData.append('file', file)
-    const url = '';
+    const url = 'https://6cd638v190.execute-api.us-east-1.amazonaws.com/Dev/files';
     const config = {
         headers: {
             'Content-Type': 'text/csv'
